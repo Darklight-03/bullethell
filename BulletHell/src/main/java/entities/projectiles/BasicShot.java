4af87ce28f6218dd9b0e4eedfc8a0e1837df8ee6
Darklight <bo.corman@gmail.com>
2015-04-30 07:53:47 -0500

    added preferences and stupid testing hitbox checks

---
+++ BulletHell/src/main/java/entities/projectiles/BasicShot.java
@@ -0,0 +1,27 @@
+package entities.projectiles;
+
+import java.awt.Rectangle;
+
+import entities.projectiles.ProjectileBase;
+
+public class BasicShot extends ProjectileBase {
+
+	protected final String NAME = "PlayerShot";
+	
+	public int size = 0;
+	
+	public BasicShot(String imageName, double x, double y, double vx0, double vy0, double ax0, double ay0){
+		super(imageName, x, y, vx0, vy0, ax0, ay0);
+		hitBox = new Rectangle((int) x - this.getImage().getWidth() / 2, (int) y - this.getImage().getHeight(), this
+				.getImage().getWidth(), this.getImage().getHeight());
+	}
+	
+	public BasicShot(String imageName, double angle, double speed, double x, double y, double ax0, double ay0, boolean toUseAnglesPutABooleanHereThatIsTrue){
+		super(imageName, angle, speed, x, y, ax0, ay0, toUseAnglesPutABooleanHereThatIsTrue);
+	}
+	
+	public String toString(){
+		return NAME+":  "+x+", "+y;
+	}
+
+}
